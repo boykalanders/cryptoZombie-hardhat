215d6821d499d0c610c0786a8f498c6d718dc4a6
Boykalanders <boykalanders@gmail.com>
2024-07-13 18:40:05 -0400

    chore: test

---
+++ test/CryptoZombie.js
@@ -0,0 +1,16 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("CryptoZombie", function () {
+    it("Deploying the CryptoZombie", async function () {
+        const [owner] = await ethers.getSigners();
+
+        const zombieContract = await ethers.deployContract("ZombieFactory");
+
+        const tx = await zombieContract.createRandomZombie("MStar");
+
+        const res = await tx.wait();
+
+        console.log(res.logs);
+    })
+})
