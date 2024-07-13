6113ff69535cabd54dba2cdb7e12d5744727a25a
Boykalanders <boykalanders@gmail.com>
2024-07-13 18:39:37 -0400

    feat: Build ZombieFactory contract

---
+++ contracts/zombiefactory.sol
@@ -0,0 +1,42 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.24;
+
+import "hardhat/console.sol";
+
+contract ZombieFactory {
+
+    event NewZombie(uint zombieLength, string zombieName, uint zombieDNA);
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    mapping (uint => address) public zombieToOwner; 
+    mapping (address => uint) ownerZombieCount;
+
+    function _createZombie(string memory _name, uint _dna) internal {
+        zombies.push(Zombie(_name, _dna));
+        zombieToOwner[zombies.length-1] = msg.sender;
+        ownerZombieCount[msg.sender]++;
+        emit NewZombie(zombies.length-1, _name, _dna);
+    }
+
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+
+}
